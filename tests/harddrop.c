a97e20e119e08c58dbc50ea47f37582f8a993d8a
R Z <91019650+RZ781@users.noreply.github.com>
2026-03-02 22:42:33 +0000

    Added test for topping out

--- tests/harddrop.c
+++ tests/harddrop.c
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include "citrus.h"
 #include "tests.h"
@@ -23,24 +24,48 @@
 void hard_drop_test(void)
 {
 	CitrusGame game;
+	// infinite o pieces
 	LoopRandomizer randomizer_data = {.length = 1,.position = 0,.pieces =
 		    (const CitrusPiece *[]) {citrus_pieces + CITRUS_COLOR_O}
 	};
 	CitrusGame_init(&game, board, next_piece_queue, test_config,
 			&randomizer_data, NULL);
 
+	// o piece is at spawn position
 	clear_board();
 	set_o_piece(4, 21, CITRUS_CELL_FULL);
 	assert_expected();
+	// o piece hard dropped at center
 	CitrusGame_key_down(&game, CITRUS_KEY_HARD_DROP);
 	set_o_piece(4, 0, CITRUS_CELL_FULL);
 	assert_expected();
+	// second o piece hard dropped at center
 	CitrusGame_key_down(&game, CITRUS_KEY_HARD_DROP);
 	set_o_piece(4, 2, CITRUS_CELL_FULL);
 	assert_expected();
+	// o piece hard dropped left of center
 	CitrusGame_key_down(&game, CITRUS_KEY_LEFT);
 	CitrusGame_key_down(&game, CITRUS_KEY_LEFT);
 	CitrusGame_key_down(&game, CITRUS_KEY_HARD_DROP);
 	set_o_piece(2, 0, CITRUS_CELL_FULL);
 	assert_expected();
+	// place o pieces to top
+	for (int i = 0; i < 8; i++) {
+		CitrusGame_key_down(&game, CITRUS_KEY_HARD_DROP);
+		set_o_piece(4, 4 + i * 2, CITRUS_CELL_FULL);
+	}
+	assert_expected();
+	if (!CitrusGame_is_alive(&game)) {
+		fprintf(stderr, "hard_drop_test(): expected alive, is dead\n");
+		abort();
+	}
+	// top out
+	CitrusGame_key_down(&game, CITRUS_KEY_HARD_DROP);
+	set_o_piece(4, 21, CITRUS_CELL_EMPTY);
+	set_o_piece(4, 20, CITRUS_CELL_FULL);
+	assert_expected();
+	if (CitrusGame_is_alive(&game)) {
+		fprintf(stderr, "hard_drop_test(): expected dead, is alive\n");
+		abort();
+	}
 }
